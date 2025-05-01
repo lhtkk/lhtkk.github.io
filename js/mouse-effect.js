e2a5f2890dbff1411aef5bafc566f5b10e8c6269
lhtkk <17779285992@163.com>
2025-05-01 16:39:17 +0800

    Site updated: 2025-05-01 16:39:17

--- js/mouse-effect.js
+++ js/mouse-effect.js
@@ -1,10 +1,11 @@
 document.addEventListener('mousemove', function(e) {
   const trail = document.createElement('div');
   trail.className = 'trail';
-  
-  // 设置渐变色
-  trail.style.background = 'linear-gradient(to right, #ff007f, #7f00ff)';
-  
+
+  // 确保为每个 trail 设置一个随机颜色
+  const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`; // 随机颜色
+  trail.style.backgroundColor = randomColor;
+
   trail.style.left = e.pageX + 'px';
   trail.style.top = e.pageY + 'px';
   
