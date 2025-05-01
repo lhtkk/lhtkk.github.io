869f5deefef3765f6a22947ec9dbca1dc9be1bdf
lhtkk <17779285992@163.com>
2025-05-01 16:36:37 +0800

    Site updated: 2025-05-01 16:36:37

--- js/mouse-effect.js
+++ js/mouse-effect.js
@@ -1,8 +1,13 @@
 document.addEventListener('mousemove', function(e) {
   const trail = document.createElement('div');
   trail.className = 'trail';
+  
+  // 设置渐变色
+  trail.style.background = 'linear-gradient(to right, #ff007f, #7f00ff)';
+  
   trail.style.left = e.pageX + 'px';
   trail.style.top = e.pageY + 'px';
+  
   document.body.appendChild(trail);
 
   setTimeout(() => {
